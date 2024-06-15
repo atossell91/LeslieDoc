cfa7c89baf63fc2cd9d1ca88e39ba3e15e273e22
Anthony Tossell <atossell91@gmail.com>
2024-06-14 17:26:53 -0700

    Setup project structure

---
+++ LeslieDoc-cli/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class Program {
+    public static void Main(String[] args) {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
